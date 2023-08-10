08136bb7e3ae083966fc3a42d480890bf2434c47
zinzindev <zinzin30@windowslive.com>
2023-08-10 23:35:55 +0700

    first setting

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const todoListRoutes = require('./routes/todoList');
+const db = require('./models');
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/todo-list', todoListRoutes);
+
+db.sequelize.sync().then(() => {
+    app.listen(8000, () => {
+        console.log(`Server is running at port 8000`);
+    });
+});
---
+++ models/TodoList.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+    const model = sequelize.define('TodoList', {
+        task: {
+            type: DataTypes.STRING(255)
+        }
+    }, {
+        tableName: 'todolists',
+        timestamps: false,
+    });
+
+    return model;
+}
